5e087f53b8107ea9e86a1a0794c970853c7c2f1e
Dani <al.danielfuster@iesramonllull.net>
2023-10-27 12:54:17 +0200

    mi tercer commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Hola mundo soy Dani Fuster Latorre")
+print("Hola mundo soy Dani Fuster Latorre")
+
+#soy duende de loco 
+print("Hola mundo soy Dani Fuster Latorre")
+
+"""
+texto que no se va a interpretar, va serios
+texto que no se va a interpretar, va serios
+texto que no se va a interpretar, va serios
+"""
+
+#variables
+
+texto = "repaso de python"
+nombre = "DANI"
+altura = "177cm"
+year = 2023
+
+print(f"{texto} - {nombre} - {altura} - {str(year)}") 
