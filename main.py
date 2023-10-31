f6cd61da43ba5f93ba2545638092fa644d105b45
daniif23 <114219009+daniif23@users.noreply.github.com>
2023-10-31 12:00:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -18,3 +18,41 @@ altura = "177cm"
 year = 2023
 
 print(f"{texto} - {nombre} - {altura} - {str(year)}") 
+print(texto + "  - "  + nombre + "  -  " + altura + "  - " + str(year)) 
+
+# Entrada
+#sitioweb = input("¿Cual es tu página web?: ")
+#print("El sitio web del usuario es: " + sitioweb)
+
+# Condiciones 
+"""
+altura = int(input("¿Cual es tu página web?: "))
+if altura >= 170:
+    print ("Eres una persona alta!!")
+else:
+    print ("Eres bajito!!")
+"""
+
+# Funciones
+"""
+var_altura = int(input("¿Cual es tu página web?: "))
+
+def mostrarAltura(altura):
+    resultado = ""
+
+    if altura >= 170:
+         resultado = "Eres una persona alta!!"
+    else:
+         resultado = "Eres bajito!!"
+    
+    return resultado
+
+print(mostrarAltura(var_altura))
+"""
+
+# Listas
+personas = ["Víctor", "Paco", "Pepe"]
+print(personas[2])
+
+for persona in personas:
+    print("-" + persona)
